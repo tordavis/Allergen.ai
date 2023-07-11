963865be2b1914bfcb9a961660e6c46790c1b586
Tori Davis <tori.davis@berkeley.edu>
2023-07-10 20:51:01 -0500

    updated streamlit app to use sample node 2 df

--- allergen_ai.py
+++ allergen_ai.py
@@ -28,54 +28,44 @@ node_1_output_exp_df = node_1_output_df.explode('model_output')
 allergen14 = ['celery','crustaceans','egg','fish','gluten','lupin','milk','molluscs','mustard','none','peanuts','sesame','soy','sulphur dioxide and sulphites','tree nuts']
 
 # pull in ingredients to allergens dataframe
-# node_2_url = 'https://drive.google.com/file/d/16-faxB25Cjb8dXmMqg1RGHgbhnUIZOF4/view?usp=sharing'
-# path = 'https://drive.google.com/uc?export=download&id='+node_2_url.split('/')[-2]
-# node_2_df = pd.read_csv(path)
-
-data = [['milk','butter','brand 1','product 1'],
-        ['milk','butter','brand 1','product 2'],
-        ['milk','butter','brand 2','product 1'],
-        ['milk','butter','brand 3','product 1'],
-        ['milk','milk','brand 1','product 1'],
-        ['milk','milk','brand 1','product 2'],
-        ['milk','milk','brand 1','product 3'],
-        ['milk','milk','brand 2','product 1'],
-        ['peanuts','peanut butter','brand 1','product 1'],
-        ['peanuts','peanut butter','brand 1','product 2'],
-        ['peanuts','peanut butter','brand 2','product 1'],
-        ['tree nuts','chopped nuts','brand 1','product 1'],
-        ['tree nuts','chopped nuts','brand 2','product 1'],
-        ['tree nuts','chopped nuts','brand 2','product 2'],
-        ['tree nuts','chopped nuts','brand 3','product 1'],
-        ]
-node_2_df = pd.DataFrame(data, columns=['Allergen','Ingredient','Brand','Product'])
+node2_sample_url = 'https://raw.githubusercontent.com/tordavis/Allergen.ai/main/open_food_facts_products_example_df.csv'
+node_2_output_df = pd.read_csv(node2_sample_url, usecols=['brands_spaces','product_name','food_spaces','allergens_from_dict'])
+node_2_output_df = node_2_output_df.rename(columns={'brands_spaces':'brand','product_name':'product','food_spaces':'ingredient','allergens_from_dict':'allergen'})
 
 def main():
     # dish = st.text_input('Please enter a dish name', 'Beef Stroganoff')
-    dish = st.selectbox('select one of the predetermined dishes',node_1_output_exp_df.title.unique())
+    # dish = st.selectbox('select one of the predetermined dishes',node_1_output_exp_df.title.unique())
+    dish = st.selectbox('select one of the predetermined dishes',['cabbage soup','baked french toast'])
 
     # just keep node 1 results that match dish selected
-    # dish_selected = node_1_output_exp_df[node_1_output_exp_df.title == dish]
-    dish_ingredients = set(node_1_output_exp_df.model_output) & set(node_2_df.Ingredient)
+    dish_selected = node_1_output_exp_df[node_1_output_exp_df.title == dish]
+    dish_ingredients = dish_selected.model_output.tolist()
+    dish_ingredients = [i.lstrip() for i in dish_ingredients]
 
-    # find the allergens that are present
-    dish_allergens = set(node_2_df.Allergen)
-    allergen_select = set(allergen14) & set(dish_allergens)
+    node_2_curated = node_2_output_df[node_2_output_df['product'].isin(dish_ingredients)]
 
-    if len(allergen_select) == 0:
-        st.write('No allergens found.')
-    else:
-        user_allergen = st.selectbox('Please select an allergen',allergen_select)
+    user_allergen = st.selectbox('Please select an allergen',allergen14)
 
-    if st.button('Predict the allergens'):
+    if st.button('Show ingredients with this allergen'):
         if dish:
             if len(node_1_output_exp_df) == 0:
                 st.write('No dishes found.')
             else:
-                final_df = node_2_df.loc[node_2_df.Allergen == user_allergen]
-                st.write('### Ingredients with Allergen Present', final_df.sort_index())
+                final_df = node_2_curated.loc[node_2_curated.allergen == user_allergen]
+                if final_df.empty:
+                    st.write('Based on the products available in our dataset, we did not find any potential ingredients in this dish with', user_allergen)
+                else:
+                    
+                    final_df.drop_duplicates()
+                    st.write('### Ingredients with Allergen Present', final_df.sort_index())
+                    # user_ingredient = st.selectbox('Please select an ingredient',allergen_df.ingredient.unique())
+                    # ingredient_df = allergen_df.loc[allergen_df.ingredient == user_ingredient]
+                    # if st.button('Show ingredients with this allergen'):
+                    #     st.write('### Ingredients with Allergen Present', ingredient_df.sort_index())
+                    # else:
+                    #     st.write('Please select an ingredient')
         else:
-            st.write('Please select a dish')
+            st.write('Please select a dish and allergen')
 
 if __name__ == '__main__':
     main()
